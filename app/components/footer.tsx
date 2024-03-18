55b8e06dba1e9e21815356ce1a534d854749a1cd
Josh Maguire <jmaguire@pcs-publishing.com>
2024-03-18 21:18:34 +0000

    updated the footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,12 +4,13 @@ export default function Footer() {
   return (
     <footer className='mb-8 px-4 text-center text-gray-500'>
       <small className='mb-2 block text-xs'>
-        &copy; 2024 Josh. All rights reserved
+        &copy; 2024 task-tracker. Built by{' '}
+        <span className='underline font-medium'>Josh Maguire</span>
       </small>
       <p className='text-xs'>
         <span className='font-semibold'>About this website:</span> built with
-        React & Next.js, TypeScript, Tailwind CSS, shadcn/ui, Xata database,
-        Vercel hosting.
+        React & Next.js, TypeScript, Tailwind CSS, shadcn/ui, Xata, hosted using
+        Vercel.
       </p>
     </footer>
   )
